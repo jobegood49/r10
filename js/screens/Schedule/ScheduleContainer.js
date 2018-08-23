436d4460f2db4edaa6dea2ae1f47bf0ea8075d3b
jonathannicolas <jonathan.s.nicolas@gmail.com>
2018-08-23 15:53:59 -0700

    Add formatted data functionality being passed to schedule component

--- js/screens/Schedule/ScheduleContainer.js
+++ js/screens/Schedule/ScheduleContainer.js
@@ -1,15 +1,36 @@
-import React, { Component } from 'react';
-import Schedule from "./Schedule"
+import React, { Component } from "react";
+import Schedule from "./Schedule";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import {Text} from "react-native";
+import {formatSessionData} from "./dataFormatHelpers"
 
 class ScheduleContainer extends Component {
-    static navigationOptions = {
-        title: "Schedule"
-      };
-    render() {
-        return (
-            <Schedule />
-        );
-    }
+  static navigationOptions = {
+    title: "Schedule"
+  };
+  render() {
+    return (
+      <Query
+        query={gql`
+          {
+            allSessions {
+              location
+              id
+              startTime
+              title
+            }
+          }
+        `}
+      >
+        {({ loading, error, data }) => {
+          if (loading) return <Text>Loading...</Text>;
+          if (error) return <Text>Error :(</Text>;
+          return <Schedule data={formatSessionData(data.allSessions)} />;
+        }}
+      </Query>
+    );
+  }
 }
 
 export default ScheduleContainer;
