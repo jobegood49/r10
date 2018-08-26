182e976fd38d1c883adb3418ac8bb3fe927cd5cf
jonathannicolas <jonathan.s.nicolas@gmail.com>
2018-08-26 09:23:00 -0700

    Amend query session to take in id passed on in navigation params

--- js/screens/Session/SessionContainer.js
+++ js/screens/Session/SessionContainer.js
@@ -4,26 +4,43 @@ import Session from "./Session";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
+const SESSION_QUERY = gql`
+  query Session($id: ID!) {
+    Session(id: $id) {
+      id
+      title
+      location
+      startTime
+      speaker {
+        name
+        bio
+      }
+    }
+  }
+`;
+
 class SessionContainer extends Component {
   render() {
     console.log(this.props.navigation.getParam("id"));
     return (
       <Query
-        query={gql`
-          {
-            Session(id: "cjh2jemtn167f0122t01busx0") {
-              location
-              description
-              startTime
-              title
-            }
-          }
-        `}
+        // query={gql`
+        //   {
+        //     Session(id: "cjh2jemtn167f0122t01busx0") {
+        //       location
+        //       description
+        //       startTime
+        //       title
+        //     }
+        //   }
+        // `}
+        query={SESSION_QUERY}
+        variables={{id: this.props.navigation.getParam("id") }}
       >
         {({ loading, error, data }) => {
           if (loading) return <Text>Loading...</Text>;
           if (error) return <Text>Error :(</Text>;
-          return <Session data= {data.Session}/>;
+          return <Session data={data.Session} />;
         }}
       </Query>
     );
